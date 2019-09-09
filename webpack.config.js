cd8f9baad3cae450aae8b656d75d585e76e3edb0
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-09-09 15:01:56 +0100

    Add webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'), // __dirname is a global in node that contains the current directory
+    filename: 'app.bundle.js'
+  }
+}
