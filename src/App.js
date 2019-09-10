43a9f44e88c13260be656f09123a0596671228c9
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-09-10 19:20:10 +0100

    Add babel plugin

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return <h1>Sup, World - this is yo dev server</h1>
+    return (
+      <div>
+        <h1>Sup, World - this is yo dev server</h1>
+        <h2>Count: {this.state.count} </h2>
+        <button onClick={() => this.setState(state => ({count: state.count + 1}))}>+
+        </button>
+        <button onClick={() => this.setState(state => ({count: state.count - 1}))}>-
+        </button>
+      </div>
+    )
+  }
+  state = {
+    count: 0
   }
 }
 
