39f8febce47fc1b9cde1c37a1d05fec0449a6525
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-09-10 20:01:16 +0100

    Add css, style loader and css loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './styles.css'
 
 ReactDOM.render(<App/>, document.getElementById('app')); // renders the App component into the DOM node targeted by ID.
 
