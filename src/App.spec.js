35af4ab25a5f5666361137f1e126ad0fcd3e07e7
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-09-11 17:28:16 +0100

    Setup global jest config

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-// import 'react-testing-library/cleanup-after-each';
 import App from './App'; 
 
 describe('App', () => {
---
+++ testSetup.js
@@ -0,0 +1,15 @@
+/***
+ *** Cut from App.spec.js
+ ***/
+
+import '@testing-library/jest-dom/extend-expect';
+
+
+/***
+ *** According to the tutorial those should look like this:
+ *** import 'jest-dom/extend-expect'
+ *** import 'react-testing-library/cleanup-after-each'
+ ***/
+
+
+
