4e516f05c4e26a4e1f7e2959112996d334dac09d
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-09-12 13:46:44 +0100

    Cleanup code for cloning

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
-const Warning = React.lazy(() => import('./Warning'));
-
 class App extends React.Component {
-  state = {
-    count: 0
-  };
-
-  increment = () => {
-    this.setState(state => ({ count: state.count + 1 }));
-  };
-
-  decrement = () => {
-    this.setState(state => ({ count: state.count - 1 }));
-  };
-
   render() {
-    const { count } = this.state;
     return (
       <div>
-        <h1>Sup, World - this is yo dev server</h1>
-        <img alt="logo" src="./log.png" />
-        <h2 className={count > 10 ? 'warning' : null}>Count: {count}</h2>
-        <button onClick={this.increment}>+</button>
-        <button onClick={this.decrement}>-</button>
-        {count > 10 ? (
-          <React.Suspense fallback={null}>
-            <Warning />
-          </React.Suspense>
-        ) : null}
+        <h1>Hello, World!</h1>
       </div>
     );
   }
